25aadc90261d68d384cf411f4cafc07a376dd2b3
Evan Fleming <evan.gordon.fleming@gmail.com>
2021-03-05 08:44:37 -0800

    traits, generics, lifetimes, testing

---
+++ generics/src/main.rs
@@ -0,0 +1,46 @@
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    let p = Point { x: 5.0, y: 2.3 };
+
+    println!("p.x = {}", p.x());
+
+    println!("distance from origin = {}", p.distance_from_origin());
+}
---
+++ traits/src/main.rs
@@ -0,0 +1,60 @@
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+pub fn notify<T>(item: &T)
+where
+    T: Summary,
+{
+    println!("Breaking news! {}", item.summarize());
+}
+
+fn main() {
+    let article = NewsArticle {
+        headline: String::from("some headline"),
+        location: String::from("some location"),
+        author: String::from("some author"),
+        content: String::from("some content"),
+    };
+
+    notify(&article);
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    notify(&tweet);
+}
