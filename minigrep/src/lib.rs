badf29af6086fe9ff4f0524f2080c9b3d814c8bd
Evan Fleming <evan.gordon.fleming@gmail.com>
2021-03-07 18:35:24 -0800

    crate publish prep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -54,14 +54,14 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     contents
         .lines()
         .filter(|line| line.contains(query))
         .collect()
 }
 
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     contents
         .lines()
         .filter(|line| line.to_lowercase().contains(query.to_lowercase().as_str()))
